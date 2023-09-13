0dd6127d330ea52563f0f8ff11003a1de237f77b
Carlos Robert <crobertdmelo@gmail.com>
2023-09-13 15:59:33 -0300

    First commit

---
+++ problem_1.js
@@ -0,0 +1,11 @@
+function multiplesOf3and5(number) {
+    let sum = 0;
+    for (let value = 3; value < number; value++) {
+        if (value % 3 === 0 || value % 5 === 0) {
+            sum += value;
+        }
+    }
+    return sum;
+}
+
+console.log(multiplesOf3and5(1000));
