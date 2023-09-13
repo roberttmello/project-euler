aa3d52c3b853ebc2b74125e9704adc85e2a86b9e
Carlos Robert <crobertdmelo@gmail.com>
2023-09-13 16:17:36 -0300

    Edit - semicolons removed

--- problem_1.js
+++ problem_1.js
@@ -1,11 +1,11 @@
 function multiplesOf3and5(number) {
-    let sum = 0;
+    let sum = 0
     for (let value = 3; value < number; value++) {
         if (value % 3 === 0 || value % 5 === 0) {
-            sum += value;
+            sum += value
         }
     }
-    return sum;
+    return sum
 }
 
-console.log(multiplesOf3and5(1000));
+console.log(multiplesOf3and5(1000))
