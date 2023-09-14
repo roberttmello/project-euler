a48795d012aafdbe14c8c841688b1f82325cb6fb
Carlos Robert <crobertdmelo@gmail.com>
2023-09-14 16:00:23 -0300

    Add - problem_2 solution added

--- problem_2.js
+++ problem_2.js
@@ -1,10 +1,22 @@
 function fiboEvenSum(n) {
-    // 1,2,3,5,8,1
-    sum = 0
-    for (let i = 1; i < n; i++) {
+    let first = 1, second = 2
+    let fibSequence = [first, second]
+    let fib = 0, sum = 0
 
+    for (let i = 0; fib < n; i++) {
+        fib = first + second
+        first = second
+        second = fib
+        fibSequence.push(fib)
     }
+
+    for (const fibValue of fibSequence) {
+        if (fibValue % 2 === 0) {
+            sum += fibValue
+        }
+    }
+
     return sum
 }
 
-fiboEvenSum(10)
+console.log(fiboEvenSum(34))
